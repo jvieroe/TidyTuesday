abf5c5551464c0ad4ab3f935bbed63fdb2adfb31
jvieroe <jvieroe@gmail.com>
2022-01-25 09:45:29 +0100

    Update code_boardgames.R

--- 2022/week_04/code_boardgames.R
+++ 2022/week_04/code_boardgames.R
@@ -1,15 +1,19 @@
 library(tidytuesdayR)
 library(tidyverse)
 
-tt <- tidytuesdayR::tt_load('2022-01-25')
+# tt <- tidytuesdayR::tt_load('2022-01-25')
+# 
+# details <- tt[[1]]
+# ratings <- tt[[2]]
+# 
+# rm(tt)
+# 
+# saveRDS(details, "2022/week_04/data/details.rds")
+# saveRDS(ratings, "2022/week_04/data/ratings.rds")
 
-details <- tt[[1]]
-ratings <- tt[[2]]
 
-rm(tt)
-
-saveRDS(details, "2022/week_04/data/details.rds")
-saveRDS(ratings, "2022/week_04/data/ratings.rds")
+details <- readRDS(details, "2022/week_04/data/details.rds")
+ratings <- readRDS(ratings, "2022/week_04/data/ratings.rds")
 
 
 tt <- details %>% 
