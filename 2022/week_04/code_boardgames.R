19a5eecccf4aa9d20d3c8eb4990f21be6d8976e5
jvieroe <jvieroe@gmail.com>
2022-01-25 09:44:51 +0100

    Update code_boardgames.R

--- 2022/week_04/code_boardgames.R
+++ 2022/week_04/code_boardgames.R
@@ -10,3 +10,8 @@ rm(tt)
 
 saveRDS(details, "2022/week_04/data/details.rds")
 saveRDS(ratings, "2022/week_04/data/ratings.rds")
+
+
+tt <- details %>% 
+  mutate(tmp = grepl("Green", boardgamedesigner, ignore.case = TRUE)) %>% 
+  filter(tmp)
