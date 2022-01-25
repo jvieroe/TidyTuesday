ba519b9b438bd6981a2931e3caba1caef3796e8d
jvieroe <jvieroe@gmail.com>
2022-01-25 09:41:54 +0100

    launch w04

---
+++ 2022/week_04/code_boardgames.R
@@ -0,0 +1,12 @@
+library(tidytuesdayR)
+library(tidyverse)
+
+tt <- tidytuesdayR::tt_load('2022-01-25')
+
+details <- tt[[1]]
+ratings <- tt[[2]]
+
+rm(tt)
+
+saveRDS(details, "2022/week_04/data/details.rds")
+saveRDS(ratings, "2022/week_04/data/ratings.rds")
