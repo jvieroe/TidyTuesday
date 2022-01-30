a5d05b3c1a3e6f902b06957ff5ac786b21fb1f10
jvieroe <jvieroe@gmail.com>
2022-01-31 00:54:52 +0100

    ignore

--- 2022/week_04/code_boardgames.R
+++ 2022/week_04/code_boardgames.R
@@ -15,8 +15,6 @@ library(tidyverse)
 details <- readRDS("2022/week_04/data/details.rds")
 ratings <- readRDS("2022/week_04/data/ratings.rds")
 
-details <- details %>% 
-  mutate(test = 1)
 
 df <- ratings %>% 
   tidylog::left_join(., details,
@@ -31,9 +29,13 @@ df <- df %>%
 
 
 df <- df %>% 
-  filter(yearpublished >= 1922) %>% 
+  filter(yearpublished >= 1921) %>% 
   filter(yearpublished <= 2021)
 
+df %>% 
+  ggplot(.) +
+  geom_point(aes(x = average, y = owned),
+             alpha = 0.3)
 
 df %>% 
   group_by(yearpublished) %>% 
@@ -41,11 +43,60 @@ df %>%
   ggplot(., aes(yearpublished, count)) +
   geom_line()
 
+df %>% 
+  filter(grepl("Dice rolling", boardgamemechanic, ignore.case = T)) %>% 
+  group_by(yearpublished) %>% 
+  summarize(count = n()) %>% 
+  ggplot(., aes(yearpublished, count)) +
+  geom_line()
+
+plot_df <- df %>% 
+  mutate(dice = grepl("Dice rolling", boardgamemechanic, ignore.case = T),
+         memory = grepl("Memory", boardgamemechanic, ignore.case = T)) %>% 
+  group_by(yearpublished) %>% 
+  summarize(count = n(),
+            dice = sum(dice),
+            memory = sum(memory)) %>% 
+  mutate(pct_dice = dice/count,
+         pct_memory = memory/count)
+
+ggplot(plot_df) +
+  geom_line(aes(yearpublished, pct_dice))
+
+
+
+df %>% 
+  group_by(yearpublished) %>% 
+  summarize(avg = mean(average)) %>% 
+  ggplot(., aes(yearpublished, avg)) +
+  geom_line() 
+
+df %>% 
+  group_by(yearpublished) %>% 
+  summarize(avg = median(playingtime)) %>% 
+  ggplot(., aes(yearpublished, avg)) +
+  geom_line() 
+
+df %>% 
+  group_by(yearpublished) %>% 
+  summarize(avg = median(maxplaytime)) %>% 
+  ggplot(., aes(yearpublished, avg)) +
+  geom_line() 
+
+
+
+
+civi <- df %>% 
+  mutate(civi = grepl("Civilization", boardgamefamily, ignore.case = TRUE)) %>% 
+  filter(civi)
+
+
+top5 <- df %>% 
+  arrange(desc(owned)) %>% 
+  head(5)
+
+ggplot(df) +
+  geom_point(aes(x = minplayers, y = minplaytime))
 
 df <- df %>% 
-  mutate(minpl = factor(minplayers))
-
-ggplot(df, aes(yearpublished, count)) +
-  geom_line() +
-  facet_wrap(.~ minplayers)
-
+  arrange(desc(bayes_average))
