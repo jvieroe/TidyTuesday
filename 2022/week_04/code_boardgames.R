550cc4fbf9b92e7df152ab0f9634496915f84227
jvieroe <jvieroe@gmail.com>
2022-01-30 23:18:48 +0100

    Update code_boardgames.R

--- 2022/week_04/code_boardgames.R
+++ 2022/week_04/code_boardgames.R
@@ -15,7 +15,12 @@ library(tidyverse)
 details <- readRDS("2022/week_04/data/details.rds")
 ratings <- readRDS("2022/week_04/data/ratings.rds")
 
+details <- details %>% 
+  mutate(test = 1)
 
-tt <- details %>% 
-  mutate(tmp = grepl("Green", boardgamedesigner, ignore.case = TRUE)) %>% 
-  filter(tmp)
+df <- ratings %>% 
+  tidylog::left_join(., ratings,
+                     by = "id")
+
+df <- df %>% 
+  filter(is.na(test))
