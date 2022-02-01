df5460595fafa97572565efc68fcf8bbd85e4c38
jvieroe <jvieroe@gmail.com>
2022-02-01 18:37:38 +0100

    Update code_boardgames.R

--- 2022/week_04/code_boardgames.R
+++ 2022/week_04/code_boardgames.R
@@ -1,5 +1,6 @@
 library(tidytuesdayR)
 library(tidyverse)
+library(cowplot)
 
 # tt <- tidytuesdayR::tt_load('2022-01-25')
 # 
@@ -20,83 +21,135 @@ df <- ratings %>%
   tidylog::left_join(., details,
                      by = "id")
 
-df %>% 
-  select(c(name, primary))
 
 
 df <- df %>% 
-  arrange(yearpublished, year)
+  rownames_to_column()
 
 
 df <- df %>% 
-  filter(yearpublished >= 1921) %>% 
-  filter(yearpublished <= 2021)
+  mutate(across(c(boardgameartist, boardgamedesigner),
+                ~ str_replace(.x, "\\[|\\]", ""))) %>% 
+  mutate(across(c(boardgameartist, boardgamedesigner),
+                ~ str_remove_all(.x, "\'"))) %>% 
+  mutate(across(c(boardgameartist, boardgamedesigner),
+                ~ str_replace(.x, "\\[|\\]", "")))
 
-df %>% 
-  ggplot(.) +
-  geom_point(aes(x = average, y = owned),
-             alpha = 0.3)
+df_long <- df %>% 
+  separate_rows(boardgameartist, sep = ",") %>% 
+  arrange(yearpublished, boardgamedesigner)
 
-df %>% 
-  group_by(yearpublished) %>% 
-  summarize(count = n()) %>% 
-  ggplot(., aes(yearpublished, count)) +
-  geom_line()
+df_long <- df_long %>% 
+  filter(boardgamedesigner != "(Uncredited)") %>% 
+  filter(!is.na(boardgamedesigner))
 
-df %>% 
-  filter(grepl("Dice rolling", boardgamemechanic, ignore.case = T)) %>% 
-  group_by(yearpublished) %>% 
-  summarize(count = n()) %>% 
-  ggplot(., aes(yearpublished, count)) +
-  geom_line()
 
-plot_df <- df %>% 
-  mutate(dice = grepl("Dice rolling", boardgamemechanic, ignore.case = T),
-         memory = grepl("Memory", boardgamemechanic, ignore.case = T)) %>% 
-  group_by(yearpublished) %>% 
-  summarize(count = n(),
-            dice = sum(dice),
-            memory = sum(memory)) %>% 
-  mutate(pct_dice = dice/count,
-         pct_memory = memory/count)
+df_designer <- df_long %>% 
+  group_by(boardgamedesigner) %>% 
+  summarise(sum_owned = sum(owned, na.rm = TRUE),
+            mean_rating = mean(average, na.rm = TRUE),
+            mean_bayes = mean(bayes_average, na.rm = TRUE),
+            games_produced = n(),
+            mean_year = mean(yearpublished, na.rm = TRUE)) %>% 
+  ungroup() %>% 
+  arrange(desc(games_produced),
+          desc(sum_owned),
+          desc(mean_rating))
 
-ggplot(plot_df) +
-  geom_line(aes(yearpublished, pct_dice))
+
+plot_df <- df_designer %>% 
+  slice_head(n = 100)
 
 
 
-df %>% 
-  group_by(yearpublished) %>% 
-  summarize(avg = mean(average)) %>% 
-  ggplot(., aes(yearpublished, avg)) +
-  geom_line() 
+ggplot(plot_df, aes(x = games_produced, y = sum_owned)) +
+  geom_point(aes(size = mean_bayes), alpha = .5)
 
-df %>% 
-  group_by(yearpublished) %>% 
-  summarize(avg = median(playingtime)) %>% 
-  ggplot(., aes(yearpublished, avg)) +
-  geom_line() 
+p1 <- last_plot()
 
-df %>% 
-  group_by(yearpublished) %>% 
-  summarize(avg = median(maxplaytime)) %>% 
-  ggplot(., aes(yearpublished, avg)) +
-  geom_line() 
+ggplot(plot_df, aes(x = log(games_produced), y = log(sum_owned))) +
+  geom_point(aes(size = mean_bayes,
+                 color = mean_year), 
+             alpha = .5)
+
+p2 <- last_plot()
 
 
 
+cat_df <- df_long %>% 
+  select(boardgamedesigner, primary, boardgamecategory) %>% 
+  separate_rows(boardgamecategory, sep = ",") %>% 
+  arrange(boardgamedesigner, primary, boardgamecategory)
 
-civi <- df %>% 
-  mutate(civi = grepl("Civilization", boardgamefamily, ignore.case = TRUE)) %>% 
-  filter(civi)
-
-
-top5 <- df %>% 
-  arrange(desc(owned)) %>% 
-  head(5)
-
-ggplot(df) +
-  geom_point(aes(x = minplayers, y = minplaytime))
-
-df <- df %>% 
-  arrange(desc(bayes_average))
+# df <- df %>% 
+#   filter(yearpublished >= 1921) %>% 
+#   filter(yearpublished <= 2021)
+# 
+# df %>% 
+#   ggplot(.) +
+#   geom_point(aes(x = average, y = owned),
+#              alpha = 0.3)
+# 
+# df %>% 
+#   group_by(yearpublished) %>% 
+#   summarize(count = n()) %>% 
+#   ggplot(., aes(yearpublished, count)) +
+#   geom_line()
+# 
+# df %>% 
+#   filter(grepl("Dice rolling", boardgamemechanic, ignore.case = T)) %>% 
+#   group_by(yearpublished) %>% 
+#   summarize(count = n()) %>% 
+#   ggplot(., aes(yearpublished, count)) +
+#   geom_line()
+# 
+# plot_df <- df %>% 
+#   mutate(dice = grepl("Dice rolling", boardgamemechanic, ignore.case = T),
+#          memory = grepl("Memory", boardgamemechanic, ignore.case = T)) %>% 
+#   group_by(yearpublished) %>% 
+#   summarize(count = n(),
+#             dice = sum(dice),
+#             memory = sum(memory)) %>% 
+#   mutate(pct_dice = dice/count,
+#          pct_memory = memory/count)
+# 
+# ggplot(plot_df) +
+#   geom_line(aes(yearpublished, pct_dice))
+# 
+# 
+# 
+# df %>% 
+#   group_by(yearpublished) %>% 
+#   summarize(avg = mean(average)) %>% 
+#   ggplot(., aes(yearpublished, avg)) +
+#   geom_line() 
+# 
+# df %>% 
+#   group_by(yearpublished) %>% 
+#   summarize(avg = median(playingtime)) %>% 
+#   ggplot(., aes(yearpublished, avg)) +
+#   geom_line() 
+# 
+# df %>% 
+#   group_by(yearpublished) %>% 
+#   summarize(avg = median(maxplaytime)) %>% 
+#   ggplot(., aes(yearpublished, avg)) +
+#   geom_line() 
+# 
+# 
+# 
+# 
+# civi <- df %>% 
+#   mutate(civi = grepl("Civilization", boardgamefamily, ignore.case = TRUE)) %>% 
+#   filter(civi)
+# 
+# 
+# top5 <- df %>% 
+#   arrange(desc(owned)) %>% 
+#   head(5)
+# 
+# ggplot(df) +
+#   geom_point(aes(x = minplayers, y = minplaytime))
+# 
+# df <- df %>% 
+#   arrange(desc(bayes_average))
