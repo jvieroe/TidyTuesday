cefebff0482f581b7fe9870f3b8a2311ccf891fd
jvieroe <jvieroe@gmail.com>
2022-02-08 22:30:26 +0100

    Create code_dogs.R

---
+++ 2022/week_05/code_dogs.R
@@ -0,0 +1,9 @@
+library(tidyverse)
+library(cowplot)
+library(ggtext)
+library(wesanderson)
+
+breed_traits <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-02-01/breed_traits.csv')
+trait_description <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-02-01/trait_description.csv')
+breed_rank_all <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-02-01/breed_rank.csv')
+
