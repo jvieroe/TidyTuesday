637a6f300fc4d7f5e669652279069253e60a29b9
jvieroe <jvieroe@gmail.com>
2022-02-24 00:05:44 +0100

    Update code_fh.R

--- 2022/week_08/code_fh.R
+++ 2022/week_08/code_fh.R
@@ -8,11 +8,146 @@ df <- df %>%
 
 df
 
-ggplot(df, aes(x = pr, y = cl, fill = region_name)) +
+df %>% 
+  filter(is.na(cl) | is.na(pr)) %>% 
+  tally()
+
+exp_val <- 0.2
+
+ggplot(df, aes(x = pr, y = cl, color = region_name, fill = region_name)) +
   geom_point(shape = 21,
              position = position_jitter(width = 0.5,
                                         height = 0.5),
-             alpha = .5) +
+             alpha = .25) +
   geom_smooth(method = "lm") +
-  facet_wrap(~ region_name) +
-  theme(legend.position = "none")
+  # scale_x_continuous(limits = c(1, 7),
+  #                    expand = c(exp_val, exp_val)) +
+  # scale_y_continuous(limits = c(1, 7),
+  #                    expand = c(exp_val, exp_val)) +
+  theme(legend.position = "none") +
+  facet_wrap(~ region_name)
+
+min(df$cl)
+max(df$cl)
+
+min(df$pr)
+max(df$pr)
+
+
+
+euro <- df %>% 
+  #filter(region_name == "Europe") %>% 
+  filter(year == 2020)
+
+df_tile <- euro %>% 
+  mutate(across(c(cl, pr),
+                ~ factor(.x)))
+
+df_tile <- df_tile %>% 
+  mutate(n_cntry = n()) %>% 
+  group_by(cl, pr, n_cntry,
+           .drop = FALSE) %>% 
+  summarize(n_group = n()) %>% 
+  ungroup()
+
+df_tile <- df_tile %>% 
+  mutate(n_cntry = mean(n_cntry, na.rm = TRUE)) %>% 
+  mutate(share = n_group / n_cntry)
+
+
+tile_line <- "white"
+
+ggplot(df_tile, aes(x = pr, y = cl, fill = share)) +
+  geom_tile(color = tile_line,
+            size = .75) +
+  coord_fixed()
+
+# ------------------------
+
+plot_df <- df %>% 
+  filter(!is.na(region_name)) %>% 
+  filter(year == 2020)
+
+plot_df <- plot_df %>% 
+  mutate(across(c(cl, pr),
+                ~ factor(.x)))
+
+df_tile <- plot_df %>% 
+  group_by(region_name) %>% 
+  mutate(n_cntry = n()) %>% 
+  ungroup()
+
+df_tile <- df_tile %>% 
+  group_by(cl, pr, region_name, n_cntry,
+           .drop = FALSE) %>% 
+  summarize(n_group = n()) %>% 
+  ungroup()
+
+df_tile <- df_tile %>% 
+  arrange(region_name, pr, cl)
+
+df_tile <- df_tile %>% 
+  group_by(region_name) %>% 
+  mutate(n_cntry = mean(n_cntry, na.rm = TRUE)) %>% 
+  ungroup() %>% 
+  mutate(share = n_group / n_cntry)
+
+
+tile_line <- "white"
+
+ggplot(df_tile, aes(x = pr, y = cl, fill = share)) +
+  geom_tile(color = tile_line,
+            size = .75) +
+  coord_fixed()
+
+
+
+
+# ------------------------
+
+plot_df <- df %>% 
+  filter(!is.na(region_name)) %>% 
+  filter(year == 2020)
+
+plot_df <- plot_df %>% 
+  mutate(across(c(cl, pr, region_name),
+                ~ factor(.x)))
+
+df_tile <- plot_df %>% 
+  group_by(region_name,
+           .drop = FALSE) %>% 
+  mutate(n_cntry = n()) %>% 
+  ungroup() %>% 
+  arrange(region_name)
+
+df_tile <- df_tile %>% 
+  #mutate(n_cntry = factor(n_cntry)) %>%
+  group_by(cl, pr, region_name, n_cntry,
+           .drop = FALSE) %>% 
+  summarize(n_group = n()) %>% 
+  ungroup() 
+
+df_tile <- df_tile %>%
+  mutate(n_cntry = as.numeric(n_cntry))
+
+df_tile <- df_tile %>% 
+  arrange(region_name, cl, pr)
+
+df_tile <- df_tile %>% 
+  group_by(region_name) %>% 
+  mutate(n_cntry = mean(n_cntry, na.rm = TRUE)) %>% 
+  ungroup()
+
+df_tile <- df_tile %>% 
+  mutate(share = n_group / n_cntry) %>% 
+  group_by(region_name) %>% 
+  mutate(vali = sum(share)) %>% 
+  ungroup()
+
+tile_line <- "white"
+
+ggplot(df_tile, aes(x = pr, y = cl, fill = share)) +
+  geom_tile(color = tile_line,
+            size = .75) +
+  coord_fixed() +
+  facet_wrap(~ region_name)
